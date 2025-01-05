e92ebbc0ee9992a35a2331bb5d5dbc904609a73b
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-05 18:29:20 +0530

    Submit

---
+++ src/customHook/useMousePosition .js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+export default function useMousePosition() {
+    const [mousePosition, setMousePositon] = useState({
+        x: 0,
+        y: 0
+    })
+
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            setMousePositon({
+                x: e.clientX,
+                y: e.clientY
+            })
+        };
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => window.removeEventListener("mousemove", handleMouseMove);
+    }, []);
+
+    return mousePosition;
+}
